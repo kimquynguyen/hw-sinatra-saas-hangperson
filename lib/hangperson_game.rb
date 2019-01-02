524486fc96ef1f8c430eadd7d374adaeff5353c0
kimquynguyen <kimquy.nguyen.3@gmail.com>
2019-01-02 07:12:05 +0000

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,14 +3,69 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  
+  # def guess(char)
+      
+   #end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  def guess(letter)
+    # raise error if argument is invalid
+    #alpha is any ASCII character
+    raise ArgumentError if (letter.to_s == '') or !(letter =~ /[[:alpha:]]/ )
+    
+    # turns this all lower case so we dont have to case check everytime
+    letter.downcase!
+    
+    # check letter and if it doesn't exist, add it in the corresponding variable
+    #include? is a function that checks to see if a variable is included in a given array
+    if @word.include?(letter) 
+      if @guesses.include?(letter)
+        return false
+      else
+        @guesses += letter
+      end
+    else 
+      if @wrong_guesses.include?(letter)
+        return false
+      else
+        @wrong_guesses += letter 
+      end
+    end
+    true
+  end
+  
+  def word_with_guesses
+    guessword=''
+    @word.each_char do |letter|
+      if @guesses.include?(letter)
+        guessword += letter
+      else
+        guessword += '-'
+      end
+    end
+    guessword
+  end
+  
+  def check_win_or_lose
+    if self.word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length == 7
+      return :lose
+    else
+      return :play
+    end
+  end
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
